4591b63f638e194f356e5043643faba05b8b2246
Pravalika08 <pravalika.bathula08@gmail.com>
2023-07-20 16:29:59 +0530

    intital commit BookStore

---
+++ src/main/java/com/rgt/test/SpringBootWithHibernatessApplication.java
@@ -0,0 +1,13 @@
+package com.rgt.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithHibernatessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithHibernatessApplication.class, args);
+	}
+
+}
